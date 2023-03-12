32748d9e7edd037b902a9a8403f3ec4a3c6ea1e4
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2023-03-13 01:05:15 +0700

    1 task works

--- main.c
+++ main.c
@@ -4,19 +4,21 @@
 #include <sys/stat.h>
 #include <string.h>
 
-void reverse_dir_name(const char *src_str, char *dest_str) {
-    unsigned int len = strlen(src_str);
+void reverse_dir_name( char *dest_str) {
+    unsigned int len = strlen(dest_str);
     int slash_pos = 0;
     for (int i = len; i > 0; i--) {
-        if (src_str[i] == '/') {
+        if (dest_str[i] == '/') {
             slash_pos = i;
             break;
         }
     }
-    strcpy(dest_str, src_str);
-    for (int i = slash_pos + 1; i < len; i++) {
+    //strcpy(dest_str, src_str);
+    for (int i = 0 ; i < (len - slash_pos -1)/2 ; i++) {
         //strcpy(dest_str[len + slash_pos - i],src_str[i]);
-        dest_str[len + slash_pos - i] = src_str[i];
+        char tmp = dest_str[i + slash_pos + 1];
+        dest_str[i + slash_pos + 1] = dest_str[len - i - 1];
+        dest_str[len - i - 1] = tmp;
     }
 }
 
@@ -46,13 +48,12 @@ char *concat_dir_and_file(const char *s1, const char *s2) {
 }
 
 void reverse_file(const char *dir_name, const char *new_dir_name, const char *file_name) {
-
     char *file_path = concat_dir_and_file(dir_name, file_name);
+    FILE *file_in = fopen(file_path, "r");
+
     char *new_file_name = malloc(sizeof(char) * strlen(file_name));
     reverse_file_name(file_name, new_file_name);
-    FILE *file_in = fopen(file_path, "r");
     char *new_file_path = concat_dir_and_file(new_dir_name, new_file_name);
-    puts(new_dir_name);
     FILE *file_out = fopen(new_file_path, "w");
 
     fseek(file_in, 0L, SEEK_END);
@@ -73,50 +74,33 @@ void reverse_file(const char *dir_name, const char *new_dir_name, const char *fi
     free(buffer);
     fclose(file_in);
     fclose(file_out);
-
 }
 
 int main(int argc, char *argv[]) {
 //   dir = opendir("./../testdir");
 //   dir = opendir("./../");
-    puts(argv[1]);
-    int arg_len = strlen(argv[1]);
-    printf("%d\n", arg_len);
 
-    char *dir_name = ( char *) malloc(sizeof(char) * arg_len );
-    char *new_dir_name = ( char *)  malloc(sizeof(char) * arg_len);
+    int arg_len = strlen(argv[1]);
+    char *dir_name = (char *)malloc(sizeof(char) * (arg_len + 1));
+    char *new_dir_name = ( char *)  malloc(sizeof(char) * (arg_len + 1));
 
     if (argc > 1) {
         strcpy(dir_name , argv[1]);
+        strcpy(new_dir_name , argv[1]);
+        dir_name[arg_len] = '\0';
+        new_dir_name[arg_len] = '\0';
     } else {
         dir_name = "/mnt/d/Antony/NSU_Education/4_Term/OS/lab3_files/testdir";
     }
-    puts("YARIK KRASAVA");
 
-  //  unsigned int len = strlen(dir_name);
-
-    reverse_dir_name(dir_name, new_dir_name);
-
+    reverse_dir_name(new_dir_name);
     mkdir(new_dir_name, 0777);
-    puts(new_dir_name);
 
-    free(dir_name);
-    dir_name = ( char *) malloc(sizeof(char) * arg_len );
-    strcpy(dir_name , argv[1]);
-    //puts("\n");
-//    if (strlen(dir_name) != strlen(new_dir_name)) {
-//        exit(1);
-//    }
     DIR* dir = opendir(dir_name);
-    puts(new_dir_name);
-
-
     if (!dir) {
         perror("diropen");
         exit(1);
     }
-
-    puts(new_dir_name);
 
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL) {
@@ -126,17 +110,9 @@ int main(int argc, char *argv[]) {
             reverse_file(dir_name, new_dir_name, entry->d_name);
         }
     }
-
-
     closedir(dir);
-
-//    FILE *file = fopen("./../testdir/testfile.txt", "r");
-//    if (!file) {
-//        perror("fileopen");
-//        exit(1);
-//    }
-//    char a = (char) fgetc(file);
-//    printf("%c", a);
+    free(dir_name);
+    free(new_dir_name);
     return 0;
 };
 
