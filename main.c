6dc9ebaea1cbefefe1de42a255810f1b83ab130f
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2023-03-14 15:59:40 +0700

    block read while inverse file

--- main.c
+++ main.c
@@ -3,29 +3,28 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <time.h>
 
-void reverse_dir_name( char *dest_str) {
+void reverse_dir_name(char *dest_str) {
     unsigned int len = strlen(dest_str);
-    int slash_pos = 0;
-    for (int i = len; i > 0; i--) {
+    unsigned int slash_pos = 0;
+    for (unsigned int i = len; i > 0; i--) {
         if (dest_str[i] == '/') {
             slash_pos = i;
             break;
         }
     }
-    //strcpy(dest_str, src_str);
-    for (int i = 0 ; i < (len - slash_pos -1)/2 ; i++) {
-        //strcpy(dest_str[len + slash_pos - i],src_str[i]);
+    for (int i = 0; i < (len - slash_pos - 1) / 2; i++) {
         char tmp = dest_str[i + slash_pos + 1];
         dest_str[i + slash_pos + 1] = dest_str[len - i - 1];
         dest_str[len - i - 1] = tmp;
     }
 }
 
-void reverse_file_name(const char *src_str,  char *dest_str) {
+void reverse_file_name(const char *src_str, char *dest_str) {
     unsigned int len = strlen(src_str);
-    int dot_pos = -1;
-    for (int i = len; i > 0; i--) {
+    unsigned int dot_pos = -1;
+    for (unsigned int i = len; i > 0; i--) {
         if (src_str[i] == '.') {
             dot_pos = i;
             break;
@@ -41,6 +40,11 @@ char *concat_dir_and_file(const char *s1, const char *s2) {
     unsigned int len1 = strlen(s1);
     unsigned int len2 = strlen(s2);
     char *result = malloc(len1 + len2 + 1);
+    if (!result) {
+        printf("Не удалось выделить память: result\n");
+        exit(1);
+    }
+
     strcpy(result, s1);
     strcpy(result + len1, "/");
     strcpy(result + len1 + 1, s2);
@@ -49,54 +53,88 @@ char *concat_dir_and_file(const char *s1, const char *s2) {
 
 void reverse_file(const char *dir_name, const char *new_dir_name, const char *file_name) {
     char *file_path = concat_dir_and_file(dir_name, file_name);
-    FILE *file_in = fopen(file_path, "r");
-
     char *new_file_name = malloc(sizeof(char) * strlen(file_name));
+    if (!new_file_name) {
+        printf("Не удалось выделить память: new_file_name\n");
+        exit(1);
+    }
     reverse_file_name(file_name, new_file_name);
     char *new_file_path = concat_dir_and_file(new_dir_name, new_file_name);
-    FILE *file_out = fopen(new_file_path, "w");
 
+    FILE *file_in = fopen(file_path, "r");
+    if (!file_in) {
+        printf("Не удалось открыть файл: file_in\n");
+        exit(1);
+    }
+    FILE *file_out = fopen(new_file_path, "w");
+    if (!file_out) {
+        printf("Не удалось открыть файл: file_out\n");
+        exit(1);
+    }
+//
     fseek(file_in, 0L, SEEK_END);
     int file_size = ftell(file_in);
     rewind(file_in);
-    char *buffer = malloc(file_size * sizeof(char));
-    fread(buffer, sizeof(char), file_size, file_in);
 
-    // Переворачивание содержимого
-    for (int i = 0; i < file_size / 2; i++) {
-        char tmp = buffer[i];
-        buffer[i] = buffer[file_size - i - 1];
-        buffer[file_size - i - 1] = tmp;
+    int buff_size = 256;
+    int count_blocks = file_size / buff_size;
+    int last_block_size = file_size % buff_size;
+    char *buffer = malloc(buff_size * sizeof(char));
+    unsigned int bytes_read = 0;
+
+
+    for(int i = count_blocks; i>=0; i--){
+        fseek(file_in, i*buff_size, SEEK_SET);
+        bytes_read = fread(buffer, sizeof(char), buff_size, file_in);
+        for (int j = 0; j < bytes_read / 2; j++) {
+            char tmp = buffer[j];
+            buffer[j] = buffer[bytes_read - j - 1];
+            buffer[bytes_read - j - 1] = tmp;
+        }
+        fwrite(buffer, sizeof(char), bytes_read, file_out);
     }
 
-    fwrite(buffer, sizeof(char), file_size, file_out);
-
+//    while (ftell(file_in) != SEEK_SET) {
+//
+//        fwrite(buffer, sizeof(char), bytes_read, file_out);
+//        fseek(file_in, -2 * buff_size, SEEK_CUR);
+//        int pos = ftell(file_in);
+//        pos++;
+//        // rewind(file_out);
+//    }
     free(buffer);
     fclose(file_in);
     fclose(file_out);
 }
 
 int main(int argc, char *argv[]) {
-//   dir = opendir("./../testdir");
-//   dir = opendir("./../");
-
-    int arg_len = strlen(argv[1]);
-    char *dir_name = (char *)malloc(sizeof(char) * (arg_len + 1));
-    char *new_dir_name = ( char *)  malloc(sizeof(char) * (arg_len + 1));
+    char *dir_name, *new_dir_name;
 
     if (argc > 1) {
-        strcpy(dir_name , argv[1]);
-        strcpy(new_dir_name , argv[1]);
+        int arg_len = strlen(argv[1]);
+        dir_name = (char *) malloc(sizeof(char) * (arg_len + 1));
+        if (!dir_name) {
+            printf("Не удалось выделить память: dir_name\n");
+            exit(1);
+        }
+        new_dir_name = (char *) malloc(sizeof(char) * (arg_len + 1));
+        if (!new_dir_name) {
+            printf("Не удалось выделить память: new_dir_name\n");
+            exit(1);
+        }
+
+        strcpy(dir_name, argv[1]);
+        strcpy(new_dir_name, argv[1]);
         dir_name[arg_len] = '\0';
         new_dir_name[arg_len] = '\0';
     } else {
-        dir_name = "/mnt/d/Antony/NSU_Education/4_Term/OS/lab3_files/testdir";
+        exit(1);
     }
 
     reverse_dir_name(new_dir_name);
     mkdir(new_dir_name, 0777);
 
-    DIR* dir = opendir(dir_name);
+    DIR *dir = opendir(dir_name);
     if (!dir) {
         perror("diropen");
         exit(1);
@@ -104,8 +142,6 @@ int main(int argc, char *argv[]) {
 
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL) {
-        printf("%lu - %s type:%d %d\n",
-               entry->d_ino, entry->d_name, entry->d_type, entry->d_reclen);
         if (entry->d_type == DT_REG) {
             reverse_file(dir_name, new_dir_name, entry->d_name);
         }
