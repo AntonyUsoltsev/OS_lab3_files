e86cb5521b6809c6b31a9ac19627c0a28e24fc21
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2023-03-18 02:01:30 +0700

    last type hav some bugs

--- task_2/task2.c
+++ task_2/task2.c
@@ -13,8 +13,9 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
     puts(argv[0]);
-  //  puts("dfd");
-//   a)
+    //  puts("dfd");
+
+//  a) создать каталог, указанный в аргументе;
     if (strcmp(argv[0], "./task2_a") == 0) {
         if (mkdir(argv[1], 0777) == -1) {
             puts("Error while creating directory");
@@ -22,7 +23,8 @@ int main(int argc, char *argv[]) {
             exit(1);
         }
     }
-//   b)
+
+//  b) вывести содержимое каталога, указанного в аргументе;
     else if (strcmp(argv[0], "./task2_b") == 0) {
         DIR *dir = opendir(argv[1]);
         if (!dir) {
@@ -35,7 +37,8 @@ int main(int argc, char *argv[]) {
             printf("%lu - %s type:%d %d\n", entry->d_ino, entry->d_name, entry->d_type, entry->d_reclen);
         }
     }
-//    c)
+
+//  c) удалить каталог, указанный в аргументе;
     else if (strcmp(argv[0], "./task2_c") == 0) {
         if (remove(argv[1]) == -1) {
             puts("Error while removing directory");
@@ -43,23 +46,25 @@ int main(int argc, char *argv[]) {
             exit(1);
         }
     }
-//    d)
+
+//  d) создать файл, указанный в аргументе;
     else if (strcmp(argv[0], "./task2_d") == 0) {
         FILE *file = fopen(argv[1], "wb+");
         if (!file) {
             puts("Error while creating file");
-            perror("createfile");
+            perror("create_file");
             fclose(file);
             exit(1);
         }
         fclose(file);
     }
-//    e)
+
+//  e) вывести содержимое файла, указанного в аргументе;
     else if (strcmp(argv[0], "./task2_e") == 0) {
         FILE *file = fopen(argv[1], "r");
         if (!file) {
             puts("Error while opening file");
-            perror("openfile");
+            perror("open_file");
             fclose(file);
             exit(1);
         }
@@ -69,37 +74,46 @@ int main(int argc, char *argv[]) {
         }
         fclose(file);
     }
-//    f)
+
+//  f) удалить файл, указанный в аргументе;
     else if (strcmp(argv[0], "./task2_f") == 0) {
         if (remove(argv[1]) == -1) {
             puts("Error while removing file");
-            perror("rmfile");
+            perror("rm_file");
             exit(1);
         }
     }
-//    g)
+
+//  g) создать символьную ссылку на файл, указанный в аргументе;
     else if (strcmp(argv[0], "./task2_g") == 0) {
         if (symlink(argv[1], "symlink_file") != 0) {
             puts("Error while creating symlink");
-            perror("crsymlink");
+            perror("cr_symlink");
             exit(1);
         }
     }
-//    h)
+
+//  h) вывести содержимое символьной ссылки, указанный в аргументе;
     else if (strcmp(argv[0], "./task2_h") == 0) {
         char *buffer = (char *) calloc(sizeof(char), BUFF_SIZE);
-        readlink(argv[1], buffer, BUFF_SIZE);
+        if (readlink(argv[1], buffer, BUFF_SIZE) == -1) {
+            puts("Error while reading symlink");
+            perror("read_symlink");
+            exit(1);
+        }
         puts(buffer);
-
-
     }
-//    i)
-    else if (strcmp(argv[0], "./task2_h") == 0) {
-        char *buffer = (char *) calloc(sizeof(char), BUFF_SIZE);
-        readlink(argv[1], buffer, BUFF_SIZE);
-        puts(buffer);
 
-        FILE *file = fopen(argv[1], "r");
+//  i) вывести содержимое файла, на который указывает символьная ссылка,  указанная в аргументе;
+    else if (strcmp(argv[0], "./task2_i") == 0) {
+        char *buffer = (char *) calloc(sizeof(char), BUFF_SIZE);
+        if (readlink(argv[1], buffer, BUFF_SIZE) == -1) {
+            puts("Error while reading symlink");
+            perror("read_symlink");
+            exit(1);
+        }
+        puts(buffer);
+        FILE *file = fopen(buffer, "r");
         if (!file) {
             puts("Error while opening file");
             perror("openfile");
@@ -113,44 +127,69 @@ int main(int argc, char *argv[]) {
         fclose(file);
 
 
-    }else {
-        puts("hi3");
     }
-//    char *dir_name, *new_dir_name;
-//    int arg_len = (int) strlen(argv[1]);
-//    dir_name = (char *) malloc(sizeof(char) * (arg_len + 1));
-//    if (!dir_name) {
-//        printf("Не удалось выделить память: dir_name\n");
-//        exit(1);
-//    }
-//    new_dir_name = (char *) malloc(sizeof(char) * (arg_len + 1));
-//    if (!new_dir_name) {
-//        printf("Не удалось выделить память: new_dir_name\n");
-//        exit(1);
-//    }
-//
-//    strcpy(dir_name, argv[1]);
-//    strcpy(new_dir_name, argv[1]);
-//    dir_name[arg_len] = '\0';
-//    new_dir_name[arg_len] = '\0';
-//
-//  //  reverse_dir_name(new_dir_name);
-//    mkdir(new_dir_name, 0777);
-//
-//    DIR *dir = opendir(dir_name);
-//    if (!dir) {
-//        perror("diropen");
-//        exit(1);
-//    }
-//
-//    struct dirent *entry;
-//    while ((entry = readdir(dir)) != NULL) {
-//        if (entry->d_type == DT_REG) {
-//        //    reverse_file(dir_name, new_dir_name, entry->d_name);
-//        }
-//    }
-//    closedir(dir);
-//    free(dir_name);
-//    free(new_dir_name);
+
+//  j) удалить символьную ссылку на файл, указанный в аргументе;
+    else if (strcmp(argv[0], "./task2_j") == 0) {
+        if (remove(argv[1]) == -1) {
+            puts("Error while removing symlink");
+            perror("rm_symlink");
+            exit(1);
+        }
+    }
+
+//  k) создать жесткую ссылку на файл, указанный в аргументе;
+    else if (strcmp(argv[0], "./task2_k") == 0) {
+        if (link(argv[1], "hardlink_file") != 0) {
+            puts("Error while creating hardlink");
+            perror("cr_hardlink");
+            exit(1);
+        }
+    }
+//  l) удалить жесткую ссылку на файл, указанный в аргументе;
+    else if (strcmp(argv[0], "./task2_l") == 0) {
+        if (remove(argv[1]) == -1) {
+            puts("Error while removing hardlink");
+            perror("rm_hardlink");
+            exit(1);
+        }
+    }
+
+//  m) вывести права доступа к файлу, указанному в аргументе и количество жестких ссылок на него;
+    else if (strcmp(argv[0], "./task2_m") == 0) {
+        struct stat fileStat;
+        if (stat(argv[1], &fileStat) < 0) {
+            perror("Error: cannot make stat file");
+            exit(1);
+        }
+        printf("Count of hardlinks: %lu\n", fileStat.st_nlink);
+
+        printf("Permissions: ");
+        printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+        printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
+        printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
+        printf((fileStat.st_mode & S_IXUSR) ? "x" : "-");
+        printf((fileStat.st_mode & S_IRGRP) ? "r" : "-");
+        printf((fileStat.st_mode & S_IWGRP) ? "w" : "-");
+        printf((fileStat.st_mode & S_IXGRP) ? "x" : "-");
+        printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
+        printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
+        printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");
+        printf("\n");
+    }
+
+//  n) изменить права доступа к файлу, указанному в аргументе.
+    else if (strcmp(argv[0], "./task2_n") == 0) {
+//        4: read permission
+//        2: write permission
+//        1: execute permission
+        if (chmod(argv[1], 0700) == -1) {
+            perror("Error changing file permissions");
+            return 1;
+        }
+    }
+    else {
+        puts("Unknown program");
+    }
     return 0;
 }
